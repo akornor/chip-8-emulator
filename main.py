fefae5e50b5ab44e205bd2ca306f34ba9a650f87
Raymond Akornor <raymondakornor@gmail.com>
2023-09-02 18:59:47 +0000

    Add registers.

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ class CPU:
 	def __init__(self):
 		self.memory = bytearray(4096)
 
+		## registers
+		self.V = [] # Vx
+		self.I = 0 # index
+		self.ST = 0 # sound timer
+		self.DT = 0 # delay timer
+		self.PC = 0 # program counter
+		self.SP = 0 # stack pointer
+		self.stack = []
+
 	def load_rom(self, path):
 		f = open(path, 'rb').read()
 		for i, v in enumerate(f):
